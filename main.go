2553c0442b9c82721b992c164b2130864a543564
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-02-10 01:16:57 -0400

    getDirs

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+	"github.com/ktappdev/filesync/getDirs"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	directory := "/Users/kentaylor/Downloads/"
+	getDirs.GetDirectories(directory)
+	// getfiles.GetFiles(directory)
+	// monitorfiles.MonitorFiles(directory)
+
+	w.ShowAndRun()
+}
