baae9857bf6720b819c1c2baa419b9af2aa1b96a
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-02-10 02:25:33 -0400

    no message

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func main() {
 	files := []models.FileInfo{
 		{"Lil King Project", 122, 147.00, "Hip-Hop", "WIP", "C#", "S", "02/06/24", time.Now(), time.Now()},
 		{"Some Name", 123, 120.0, "Genre", "Status", "Key", "Grade", "Release Date", time.Now(), time.Now()},
+		{"Soulful Journey", 232, 109.44, "Jazz", "Upcoming", "Bb", "S", "02/06/24", time.Now(), time.Now()},
+		{"Retro Vibes", 232, 157.41, "Electronic", "Released", "Cb", "D", "07/08/24", time.Now(), time.Now()},
+		{"Soulful Journey", 111, 150.37, "Electronic", "Upcoming", "E#", "B", "01/01/24", time.Now(), time.Now()},
+		{"Lil King Project", 736, 143.54, "Classical", "Upcoming", "Cb", "D", "01/01/24", time.Now(), time.Now()},
 	}
 	w := ui.NewFileManagerUI(a, files)
 
