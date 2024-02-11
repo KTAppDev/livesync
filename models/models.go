c95ccc44cef02d235d933ef6c50ea9de9ff1f39f
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-02-11 09:02:37 -0400

    added a struct with default values, use constructor to add values

--- models/models.go
+++ models/models.go
@@ -1,16 +1,40 @@
 package models
 
-import "time"
+import (
+	"io/fs"
+	"time"
+)
 
+// FileInfo represents information about a file.
 type FileInfo struct {
-	Name        string
-	Size        int
-	BPM         float64
-	Genre       string
-	Status      string
-	Key         string
-	Grade       string
-	ReleaseDate string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	Name        string      // File name
+	Size        int64       // File size in bytes
+	BPM         float64     // Beats per minute
+	Genre       string      // Genre of the file
+	Status      string      // Status of the file
+	Key         string      // Key of the file
+	Grade       string      // Grade of the file
+	ReleaseDate string      // Release date of the file
+	CreatedAt   time.Time   // Creation time of the file
+	UpdatedAt   time.Time   // Last update time of the file
+	Path        string      // File path
+	Permissions fs.FileMode // File permissions
+	Modified    time.Time   // Last modified time
+}
+
+// NewFileInfoWithDefaults creates a new FileInfo instance with default values.
+func (fi *FileInfo) NewFileInfoWithDefaults() {
+	fi.Name = ""
+	fi.Size = 0
+	fi.BPM = 0
+	fi.Genre = ""
+	fi.Status = ""
+	fi.Key = ""
+	fi.Grade = ""
+	fi.ReleaseDate = ""
+	fi.CreatedAt = time.Time{}
+	fi.UpdatedAt = time.Time{}
+	fi.Path = ""
+	fi.Permissions = 0
+	fi.Modified = time.Time{}
 }
