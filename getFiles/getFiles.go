c8d87fb8553aaada26f99d024abbacf8f9ba18a7
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-02-11 21:17:09 -0400

    getfiles returning date created and mod along with file name to UI

--- getFiles/getFiles.go
+++ getFiles/getFiles.go
@@ -1,9 +1,14 @@
 package getFiles
 
 import (
-	"github.com/ktappdev/filesync/models" // Assuming the FileInfo struct and related logic are in the model package
+	"fmt"
 	"io/fs"
 	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/ktappdev/filesync/models"
 )
 
 func GetFiles(cwd string) ([]models.FileInfo, error) {
@@ -18,11 +23,18 @@ func GetFiles(cwd string) ([]models.FileInfo, error) {
 			if err != nil {
 				return err
 			}
-			fileInfo := models.FileInfo{
-				Path:        path,
-				Size:        info.Size(),
-				Permissions: info.Mode(),
-				Modified:    info.ModTime(),
+			fileInfo := models.FileInfo{}
+			fileInfo.NewFileInfoWithDefaults()
+			fileInfo.Name = strings.SplitAfter(info.Name(), ".")[0][:len(strings.SplitAfter(info.Name(), ".")[0])-1] //This just removes the .extension part
+			fileInfo.Path = path
+			fileInfo.Size = info.Size()
+			fileInfo.Permissions = info.Mode()
+			fileInfo.Modified = info.ModTime()
+			if stat, ok := info.Sys().(*syscall.Stat_t); ok {
+				t := time.Unix(stat.Birthtimespec.Sec, 0)
+				fileInfo.CreatedAt = t
+			} else {
+				fmt.Println("System specific info could not be asserted to *syscall.Stat_t")
 			}
 			filesInfo = append(filesInfo, fileInfo)
 		}
