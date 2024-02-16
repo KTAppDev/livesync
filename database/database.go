c57a07c56a0af32e8152e692db0f3c2b9f7f0e9c
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-02-16 14:01:07 -0400

    added sqlite db

---
+++ database/database.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/ktappdev/filesync/models"
+)
+
+func InitDB(db *sql.DB) error {
+	// Create the 'files' table
+	createTableSQL := `CREATE TABLE IF NOT EXISTS files (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        size INTEGER,
+        bpm REAL,
+        genre TEXT,
+        status TEXT,
+        key TEXT,
+        grade TEXT,
+        release_date TEXT,
+        created_at TEXT,
+        modified_at TEXT
+    );`
+
+	_, err := db.Exec(createTableSQL)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Database and table created successfully.")
+	return nil
+}
+
+// Function to load files from the database
+func LoadFilesFromDB(db *sql.DB) ([]models.FileInfo, error) {
+	var files []models.FileInfo
+	rows, err := db.Query("SELECT name, size, bpm, genre, status, key, grade, release_date, created_at, modified_at FROM files")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var file models.FileInfo
+		err := rows.Scan(&file.Name, &file.Size, &file.BPM, &file.Genre, &file.Status, &file.Key, &file.Grade, &file.ReleaseDate, &file.CreatedAt, &file.Modified)
+		if err != nil {
+			return nil, err
+		}
+		files = append(files, file)
+	}
+	return files, nil
+}
--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	// "time"
+	"log"
 
 	"fyne.io/fyne/v2/app"
 	// "github.com/ktappdev/filesync/data"
 	// "github.com/ktappdev/filesync/getDirs"
+	"github.com/ktappdev/filesync/database"
 	"github.com/ktappdev/filesync/getFiles"
 	"github.com/ktappdev/filesync/monitorFiles"
 	"github.com/ktappdev/filesync/ui"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	// files := data.GetFakeFiles()
 	directory := "/Users/kentaylor/Downloads/"
+	// Open a connection to the SQLite database file
+	db, err := sql.Open("sqlite3", "./file_manager.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Initialize the database
+	err = database.InitDB(db)
+	if err != nil {
+		log.Fatal("Error initializing database: ", err)
+	}
+
 	// getDirs.GetDirectories(directory)
 	allFiles, err := getFiles.GetFiles(directory)
 	if err != nil {
