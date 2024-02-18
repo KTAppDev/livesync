3782b20951e289ff8f23f3d4b8ed9ba619e44b0b
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-02-18 13:53:11 -0400

    gets relative path + cwd to get path of files in sub dirs

--- getFiles/getFiles.go
+++ getFiles/getFiles.go
@@ -20,23 +20,33 @@ func GetFiles(cwd string) ([]models.FileInfo, error) {
 			return err
 		}
 		if !d.IsDir() {
+			relPath, err := filepath.Rel(cwd, path)
+			if err != nil {
+				return err
+			}
+
 			info, err := d.Info()
 			if err != nil {
 				return err
 			}
+
+			// fmt.Println(cwd + relPath)
+
 			fileInfo := models.FileInfo{}
 			fileInfo.NewFileInfoWithDefaults()
-			fileInfo.Name = strings.SplitAfter(info.Name(), ".")[0][:len(strings.SplitAfter(info.Name(), ".")[0])-1] //This just removes the .extension part
-			fileInfo.Path = path
-			fileInfo.Size = uint32(info.Size()) // uint32 because this is 4gb thats big enough
+			fileInfo.Name = strings.SplitAfter(info.Name(), ".")[0][:len(strings.SplitAfter(info.Name(), ".")[0])-1]
+			fileInfo.Path = cwd + relPath
+			fileInfo.Size = uint32(info.Size())
 			fileInfo.Permissions = info.Mode()
 			fileInfo.ModifiedAt = info.ModTime()
+
 			if stat, ok := info.Sys().(*syscall.Stat_t); ok {
 				creationTime := time.Unix(stat.Birthtimespec.Sec, 0)
 				fileInfo.CreatedAt = creationTime
 			} else {
 				fmt.Println("System specific info could not be asserted to *syscall.Stat_t")
 			}
+
 			filesInfo = append(filesInfo, fileInfo)
 		}
 		return nil
