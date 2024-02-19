cbc7b7ca558a7eb48926fa4ca2373888583ee2a0
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-02-19 19:27:59 -0400

    not needed right now

--- getDirs/getDirs.go
+++ getDirs/getDirs.go
@@ -1,51 +1,51 @@
 package getDirs
 
-import (
-	"fmt"
-	"os"
-	"syscall"
+// import (
+// 	"fmt"
+// 	"os"
+// 	"syscall"
 
-	"github.com/ktappdev/filesync/unixTimeConverter"
-)
+// 	"github.com/ktappdev/filesync/unixTimeConverter"
+// )
 
-func GetDirectories(cwd string) {
+// func GetDirectories(cwd string) {
 
-	entries, err := os.ReadDir(cwd)
-	if err != nil {
-		fmt.Printf("Error reading directory: %v\n", err)
-		return
-	}
+// 	entries, err := os.ReadDir(cwd)
+// 	if err != nil {
+// 		fmt.Printf("Error reading directory: %v\n", err)
+// 		return
+// 	}
 
-	for _, entry := range entries {
-		if entry.IsDir() {
-			fmt.Println("Directory name:", entry.Name()) // Print the directory name
+// 	for _, entry := range entries {
+// 		if entry.IsDir() {
+// 			fmt.Println("Directory name:", entry.Name()) // Print the directory name
 
-			info, err := entry.Info() // Get the FileInfo object
-			if err != nil {
-				fmt.Printf("Error reading directory: %v\n", err)
-				return
-			}
+// 			info, err := entry.Info() // Get the FileInfo object
+// 			if err != nil {
+// 				fmt.Printf("Error reading directory: %v\n", err)
+// 				return
+// 			}
 
-			fmt.Println("START DIR STATS")
-			fmt.Println("Size:", info.Size())                     // Size in bytes
-			fmt.Println("Permissions:", info.Mode())              // File permissions
-			fmt.Println("Last Modified:", info.ModTime())         // Last modification time
-			fmt.Println("Is Directory:", info.IsDir())            // Is it a directory?
-			fmt.Printf("System specific info: %+v\n", info.Sys()) // OS-specific info; might be nil
+// 			fmt.Println("START DIR STATS")
+// 			fmt.Println("Size:", info.Size())                     // Size in bytes
+// 			fmt.Println("Permissions:", info.Mode())              // File permissions
+// 			fmt.Println("Last Modified:", info.ModTime())         // Last modification time
+// 			fmt.Println("Is Directory:", info.IsDir())            // Is it a directory?
+// 			fmt.Printf("System specific info: %+v\n", info.Sys()) // OS-specific info; might be nil
 
-			if stat, ok := info.Sys().(*syscall.Stat_t); ok {
-				fmt.Println("Device:", stat.Dev)
-				fmt.Println("Inode:", stat.Ino)
-				fmt.Println("UID:", stat.Uid)
-				fmt.Println("GID:", stat.Gid)
-				fmt.Println("Creation Date: ", unixTimeConverter.ConvertUnixTimeToReadable(stat.Birthtimespec.Sec))
-				fmt.Println("Modified Date: ", unixTimeConverter.ConvertUnixTimeToReadable(stat.Mtimespec.Sec))
-				fmt.Println("END DIR STATS")
-				fmt.Println("")
-			} else {
-				fmt.Println("System specific info could not be asserted to *syscall.Stat_t")
-			}
-		}
-	}
+// 			if stat, ok := info.Sys().(*syscall.Stat_t); ok {
+// 				fmt.Println("Device:", stat.Dev)
+// 				fmt.Println("Inode:", stat.Ino)
+// 				fmt.Println("UID:", stat.Uid)
+// 				fmt.Println("GID:", stat.Gid)
+// 				fmt.Println("Creation Date: ", unixTimeConverter.ConvertUnixTimeToReadable(stat.Birthtimespec.Sec))
+// 				fmt.Println("Modified Date: ", unixTimeConverter.ConvertUnixTimeToReadable(stat.Mtimespec.Sec))
+// 				fmt.Println("END DIR STATS")
+// 				fmt.Println("")
+// 			} else {
+// 				fmt.Println("System specific info could not be asserted to *syscall.Stat_t")
+// 			}
+// 		}
+// 	}
 
-}
+// }
